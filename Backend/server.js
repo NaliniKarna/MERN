7c7f7523086796ef332077912b01746f85baabc8
Nalini Karna <knalini23@tbc.edu.np>
2025-08-08 20:21:45 +0545

    imported router file and loaded the application.

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,15 @@
 import express from "express";
-require('dotenv').config();
+import dotenv from "dotenv";
+//import the router file of item
+import item from './routes/item.js';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-    res.status(200). json({
-        message: "request handled successfully"
-    });
-});
-
-app.post('/items', (req,res) => {
-    res.send("Got a POST request");
-});
-
-app.put('/items/:id', (req,res)=> {
-    res.send("Got a PUT request");
-});
-
-app.delete('/items/:id', (req,res)=> {
-    res.send("Got a DELETE request");
-});
+//load into application
+app.use('/', item);
+app.use('/api', item);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
